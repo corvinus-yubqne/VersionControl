5e1c0ffc3a5580dedb443008a56082e1fcb22c1a
Patrik Zakar <patrik.zakar@stud.uni-corvinus.hu>
2021-10-08 12:42:15 +0200

    Created CreateTable()

--- UserMaintenance/excel_export/Form1.cs
+++ UserMaintenance/excel_export/Form1.cs
@@ -25,11 +25,55 @@ namespace excel_export
         {
             InitializeComponent();
             LoadData();
+            CreateTable();
+
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
 
         private void LoadData()
         {
             Flats = context.Flats.ToList();
         }
+
+        private void CreateTable()
+        {
+            string[] headers = new string[]
+            {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter Ár (Ft/m2)"
+            };
+        }
+
+        
+        
+            
+        
     }
 }
