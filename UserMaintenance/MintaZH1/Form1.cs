48eca2c92a646c1b08de51a3e60cc3009af30c48
Patrik Zakar <patrik.zakar@stud.uni-corvinus.hu>
2021-10-13 13:21:22 +0200

    Added in the Excel template

--- UserMaintenance/MintaZH1/Form1.cs
+++ UserMaintenance/MintaZH1/Form1.cs
@@ -7,14 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace MintaZH1
 {
     public partial class Form1 : Form
     {
+        List<OlympicResult> results = new List<OlympicResult>();
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void ExcelExport()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
+        private void LoadData (string filename)
+        {
+
         }
     }
 }
