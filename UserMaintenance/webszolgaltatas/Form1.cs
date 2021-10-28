1ee63113d1f6f311ddae3ba34371363031072067
Patrik Zakar <patrik.zakar@stud.uni-corvinus.hu>
2021-10-28 13:13:50 +0200

    Created the process for XML document

--- UserMaintenance/webszolgaltatas/Form1.cs
+++ UserMaintenance/webszolgaltatas/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using webszolgaltatas.MnbServiceReference;
 using webszolgaltatas.Entities;
+using System.Xml;
 
 namespace webszolgaltatas
 {
@@ -21,9 +22,11 @@ namespace webszolgaltatas
             WebServiceCall();
 
             dataGridView1.DataSource = Rates;
+
+            
         }
 
-        private void WebServiceCall()
+        public void WebServiceCall()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
 
@@ -37,6 +40,24 @@ namespace webszolgaltatas
             var response = mnbService.GetExchangeRates(request);
 
             var result = response.GetExchangeRatesResult;
+
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                RateData rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0) rate.Value = value / unit;
+            }
         }
     }
 }
