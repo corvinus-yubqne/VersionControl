30d9abaeddd37af4d7d29b335fee0818780d7d1c
Patrik Zakar <patrik.zakar@stud.uni-corvinus.hu>
2021-11-18 13:53:54 +0100

    Car class befejezve

--- UserMaintenance/gyartosor/Entities/Car.cs
+++ UserMaintenance/gyartosor/Entities/Car.cs
@@ -12,7 +12,8 @@ namespace gyartosor.Entities
     {
         protected override void DrawImage(Graphics g)
         {
-            throw;
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
         }
     }
 }
