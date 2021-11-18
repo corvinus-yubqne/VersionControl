4c85d14d4fab6c482844cfea3bdb8a10cd6cd541
Patrik Zakar <patrik.zakar@stud.uni-corvinus.hu>
2021-11-18 14:34:03 +0100

    Labdaszínezés megoldása

--- UserMaintenance/gyartosor/Form1.cs
+++ UserMaintenance/gyartosor/Form1.cs
@@ -84,7 +84,10 @@ namespace gyartosor
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Factory = new BallFactory();
+            Factory = new BallFactory
+            {
+                BallColor = button3.BackColor
+            };
         }
 
         private void button3_Click(object sender, EventArgs e)
