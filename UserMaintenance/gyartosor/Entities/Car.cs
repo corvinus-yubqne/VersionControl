5c9e5419c76f616448af9ae0df4e9b778f54a961
Patrik Zakar <patrik.zakar@stud.uni-corvinus.hu>
2021-11-18 13:45:00 +0100

    Car class létrehozva

---
+++ UserMaintenance/gyartosor/Entities/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using gyartosor.Abstracts;
+
+namespace gyartosor.Entities
+{
+    public class Car : Toy
+    {
+
+    }
+}
