1a527c3c2c1f1bdd66986a19a24dc1514442371c
Patrik Zakar <patrik.zakar@stud.uni-corvinus.hu>
2021-11-18 13:19:09 +0100

    createTimer eseménykiszolgáló

--- UserMaintenance/gyartosor/Form1.cs
+++ UserMaintenance/gyartosor/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyartosor.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,32 @@ namespace gyartosor
 {
     public partial class Form1 : Form
     {
+        List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _factory;
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+
+        }
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            mainPanel.Controls.Add(ball);
+            ball.Left = -Width;
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+
         }
     }
 }
