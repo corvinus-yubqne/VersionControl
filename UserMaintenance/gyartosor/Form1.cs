3943c91d954f149fec25c143296190f32fa66644
Patrik Zakar <patrik.zakar@stud.uni-corvinus.hu>
2021-11-18 14:28:35 +0100

    colorPicker kész

--- UserMaintenance/gyartosor/Form1.cs
+++ UserMaintenance/gyartosor/Form1.cs
@@ -86,5 +86,20 @@ namespace gyartosor
         {
             Factory = new BallFactory();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+
+            colorPicker.Color = button.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            } else
+            {
+                button.BackColor = colorPicker.Color;
+            }
+        }
     }
 }
