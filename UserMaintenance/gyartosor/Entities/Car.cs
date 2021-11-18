12d1c40eb5219b52fc776872e61d6264db012e20
Patrik Zakar <patrik.zakar@stud.uni-corvinus.hu>
2021-11-18 13:48:44 +0100

    Images folder

--- UserMaintenance/gyartosor/Entities/Car.cs
+++ UserMaintenance/gyartosor/Entities/Car.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace gyartosor.Entities
 {
     public class Car : Toy
     {
-
+        protected override void DrawImage(Graphics g)
+        {
+            throw;
+        }
     }
 }
