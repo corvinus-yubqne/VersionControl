fefd40ac342a3db9752ec32a0eb14afedbe0361a
Patrik Zakar <patrik.zakar@stud.uni-corvinus.hu>
2021-11-26 09:21:19 +0100

    Added Mock to Test

--- UserMaintenance/UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UserMaintenance/UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -97,5 +97,30 @@ namespace UnitTestExample.Test
             }
 
         }
+
+
+        [
+            Test,
+            TestCase("irf@uni-corvinus.hu", "Abcd1234")
+        ]
+        public void TestRegisterApplicationException(string newEmail, string newPassword)
+        {
+            var accountServiceMock = new Mock<IAccountManager>(MockBehavior.Strict);
+            accountServiceMock
+                .Setup(m => m.CreateAccount(It.IsAny<Account>()))
+                .Throws<ApplicationException>();
+            var accountController = new AccountController();
+            accountController.AccountManager = accountServiceMock.Object;
+
+            try
+            {
+                var actualResult = accountController.Register(newEmail, newPassword);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOf<ApplicationException>(ex);
+            }
+        }
     }
 }
