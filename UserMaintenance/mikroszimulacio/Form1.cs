4be70b507cce8c1efdfca68fdb07e0fd8571942b
Patrik Zakar <patrik.zakar@stud.uni-corvinus.hu>
2021-12-03 16:50:56 +0100

    GetPopulation létrehozva

--- UserMaintenance/mikroszimulacio/Form1.cs
+++ UserMaintenance/mikroszimulacio/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using mikroszimulacio.Entities;
 
 namespace mikroszimulacio
 {
@@ -14,7 +16,32 @@ namespace mikroszimulacio
     {
         public Form1()
         {
+            List<Person> Population = new List<Person>();
+            List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+            List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+
             InitializeComponent();
+        }
+
+        public List<Person> GetPopulation(string csvpath)
+        {
+            List<Person> population = new List<Person>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while(!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(',');
+                    population.Add(new Person()
+                    {
+                        BirthYear = int.Parse(line[0]),
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                        NbrOfChildren = int.Parse(line[2])
+                    });
+                }
+            }
+
+            return population;
         }
     }
 }
